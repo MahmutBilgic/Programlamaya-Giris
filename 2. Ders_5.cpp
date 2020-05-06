16f7c5843b5bd6e3124c8aca12aed96affcfcae7
Mahmut <64757353+MahmutBilgic@users.noreply.github.com>
2020-05-07 02:27:31 +0300

    Add files via upload

---
+++ 2. Ders_5.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+	
+	float pi=3.14;
+	int a;
+	
+	printf("%d \n",(int)pi);
+	printf("%f \n",pi);
+	a=pi;
+	printf("%d \n",a);
+	a=2.15+3;
+	pi=2.15+3;
+	
+ printf("%d \n%f",a,pi);
+}
